d42c7607de947221ad4d4d164e61faceb5195c01
Saad Tazi <saad@saadtazi.com>
2014-10-03 23:43:34 -0400

    added localStorage and likes

--- server/public/scripts/main.js
+++ server/public/scripts/main.js
@@ -1,5 +1,5 @@
-/* global Backbone,Bloodhound, _, MyApp */
-$(function() {
+/* global Backbone,Bloodhound, _, MyApp, store, ga */
+$(function () {
   'use strict';
 
   var PackageStat = Backbone.Model.extend({
@@ -9,8 +9,40 @@ $(function() {
   });
 
   var PackageStats = Backbone.Collection.extend({
-    model: PackageStat
+    likedStoreKey: 'likedPackages',
+    model: PackageStat,
+    loadLiked: function () {
+      var likedPackages = store.get(this.likedStoreKey);
+      if (likedPackages) {
+        var self = this;
+        _.each(likedPackages, function (likedPackage) {
+          var pkg = new PackageStat(_.extend(likedPackage, {
+            liked: true
+          }));
+          pkg.fetch().then(function () {
+            self.add(pkg);
+          });
+        });
+      }
+    },
+    saveLiked: function () {
+      store.set(
+        this.likedStoreKey,
+        this.chain()
+        .filter(function (pkg) {
+          return pkg.get('liked') === true;
+        })
+        .map(function (pkg) {
+          return {
+            name: pkg.get('name'),
+            liked: pkg.get('liked')
+          };
+        })
+        .value()
+      );
+    }
   });
+
 
   var SearchView = Backbone.View.extend({
     // does not work, not sure why...
@@ -20,7 +52,7 @@ $(function() {
       'submit #search-form': 'formSubmitted'
     },
 
-    packageSelected: function(e, item) {
+    packageSelected: function (e, item) {
       this.sendPackage(item.value);
     },
 
@@ -29,8 +61,10 @@ $(function() {
     },
 
     sendPackage: function (packageName) {
-      var packageStat = new PackageStat({name: packageName});
-      packageStat.fetch().then(_.bind(function() {
+      var packageStat = new PackageStat({
+        name: packageName
+      });
+      packageStat.fetch().then(_.bind(function () {
         this.trigger('packageSelected', packageStat);
       }, this));
     },
@@ -43,9 +77,11 @@ $(function() {
         // prefetch: '../data/films/post_1960.json',
         remote: {
           url: '/api/packages?q=%QUERY',
-          ajax: {global: false},
-          filter: function(parsedResponse) {
-              return parsedResponse && parsedResponse.items;
+          ajax: {
+            global: false
+          },
+          filter: function (parsedResponse) {
+            return parsedResponse && parsedResponse.items;
           }
         }
       });
@@ -61,12 +97,11 @@ $(function() {
 
   var PackageView = Backbone.View.extend({
     events: {
-      'click .delete': 'clear'
+      'click .delete': 'clear',
+      'click .likedBtn': 'like'
     },
-    // tagName: 'li',
-    // className: 'list-group-item',
 
-    initialize: function() {
+    initialize: function () {
       this.listenTo(this.model, 'destroy', this.remove);
     },
     render: function () {
@@ -75,7 +110,17 @@ $(function() {
     },
     clear: function () {
       // we don't have a .remove() (api end point to delete...)
+      ga('send', 'event', 'package', 'removed', this.model.get('name'));
+
       this.model.trigger('destroy', this.model, this.model.collection);
+    },
+    like: function () {
+      var newLiked = !this.model.get('liked'),
+        eventAction = newLiked ? 'liked' : 'unliked';
+      ga('send', 'event', 'like', eventAction, this.model.get('name'));
+      this.model.set('liked', !this.model.get('liked'));
+      this.$el.find('.likedBtn').toggleClass('liked');
+
     }
   });
 
@@ -86,7 +131,9 @@ $(function() {
       this.listenTo(this.collection, 'add', this.add);
     },
     add: function (pkg) {
-      var packageView = new PackageView({model: pkg});
+      var packageView = new PackageView({
+        model: pkg
+      });
       this.$el.prepend(packageView.render().$el);
     }
 
@@ -95,30 +142,42 @@ $(function() {
 
   var AppView = Backbone.View.extend({
     el: '#app',
-    initialize: function() {
+    initialize: function () {
       var spinner = $('#spinner-modal');
-      this.searchView = new SearchView({ el: '#search-input' });
+      this.searchView = new SearchView({
+        el: '#search-input'
+      });
       this.searchView.on('packageSelected', this.addPackage, this);
 
-      this.packageList = new PackageStats([]);
-      this.listView = new PackageListView({collection: this.packageList});
+      this.packageList = new PackageStats();
+      this.listView = new PackageListView({
+        collection: this.packageList
+      });
+
+      this.packageList.loadLiked();
 
       // spinner
-      $(document).ajaxStart(function(){
+      $(document).ajaxStart(function () {
         spinner.modal('show');
       });
-      $(document).ajaxComplete(function(){
+      $(document).ajaxComplete(function () {
         spinner.modal('hide');
       });
 
+      this.listenTo(this.packageList, 'change:liked', _.bind(this.likedChanged, this));
+    },
+
+    likedChanged: function () {
+      this.packageList.saveLiked();
     },
 
     addPackage: function (pkg) {
+      ga('send', 'event', 'package', 'added', pkg.get('name'));
       this.packageList.add(pkg);
     }
 
   });
 
-  new AppView();
+  window.app = new AppView();
 
 });
